d462383f1a7770a1f1be6aae0b32578286af4205
dudeguy <3403>
2016-10-25 00:51:51 -0400

    auto-commit hackybook October 25, 2016 12:51 AM

--- console/main.swift
+++ console/main.swift
@@ -4,29 +4,35 @@
 //
 //  Created by Dude Guy  on 10/18/16.
 //
+// Don't forget to use `typealias this` after ever struct inline
+// Don't forget to use /* MaRK: */ /// Explanation:
+// Don't forget quick help is OPT+V
+//
 
 import SpriteKit
 
-/// A container for our structs
-struct s {
-	
-	// MARK: - Structs: -
-	
-	/// Object for all scenes:
-	struct ButtonNode {
-		let on_click: ()->()
-		let text: String
+// MARK: TOP
+
+/* MARK: - [V] glMoney */ /** How much $ we have */			var glMoney: Int = 0 //MARK: -
+
+/** Object for all scenes: */	struct ButtonNode {
 		
-		// Mutable for SK reasons:
-		let node: SKNode
-	}
-	
-	/// Lists for scene1: Keep scoring / Cash out
-	struct ListOfButtonNodesForScene1 {
+		/** Code that is run: */		let on_click: ()->()
 		
-		/// Says "Keep Scoring!" with current amount of $ left before you can cash out
+		/** Text displayed... */		let text: String
+			
+		/** Mutable for SK r: */		let node: SKNode
+															}
+
+/// Lists for scene1: Keep scoring / Cash out -- IMMUTABLE -- use Init for new shit
+	struct ListOfButtonNodesForScene1 { typealias this = ListOfButtonNodesForScene1
+		
+		/// Amount of $ you need before the button2 will show - IA
+		private static let amount_needed = 50
+		
+		/* MARK: */		/** Says "Keep Scoring!" with current amount of $ left before you can cash out - IA*/
 		let button1: ButtonNode
-		func makeButton1(money_left: String) -> ButtonNode {
+		private static func makeButton1(money_left: Int) -> ButtonNode {
 			let the_text = "Keep Scoring! \(money_left) left"
 			return (
 				ButtonNode( on_click: {print("You need more $")},
@@ -35,72 +41,61 @@ struct s {
 			)
 		}
 		
-		/// Says "Cash Out" with current amount of $
+		/* MARK: */		/** Says "Cash Out" with current amount of $ - IA */
 		let button2: ButtonNode
-		func makeButton2(current_cash: Int) -> ButtonNode {
+		private static func makeButton2(current_cash: Int) -> ButtonNode {
 			let the_text = "Cash Out \(current_cash)"
 			return (
 				ButtonNode( on_click: {print("You can stop now or keep scoring...")},
 										text: the_text,
 										node: SKLabelNode(text: the_text))
 			)
-		
+			
 		}
 		
-		/// Only-used-here alias for ButtonNode
-		typealias BN = ButtonNode
-		
-		/// Basic init
-		init(butter1: BN) {
-			button2_text = b2text
+		/* MARK: */		/** Basic init -- uses Globals!!! butter# = variable to init the nodes.. */
+		init(butter1: ButtonNode = this.makeButton1(this.amount_needed - glMoney),
+				 butter2: ButtonNode = this.makeButton2(glMoney))	{
+			
+			// Assign:
+			button1 = butter1
+			button2 = butter2
 		}
 	}
-	
-	/// List of lists... Mutable
-	struct GlobalNodes {
-		var scene1_buttons = ListOfButtonNodesForScene1()
+
+
+// MARK: - STRUCT GlobalNodes (MA): -
+	/** List of lists... - MA */	struct GlobalNodes {
 		
-		//var scene2_buttons = ListOfbuttonscene2
+		/** Buttons for scene1: */	var scene1_buttons = ListOfButtonNodesForScene1()
+																//var scene2_buttons = ListOfbuttonscene2
 	}
-}
 
 
-
-/// Global
-struct g {
-	
-	// MARK: - GLOBAL VARS!!! -
-	
-	/// Accessor to List of Lists
-	static var nodes = s.GlobalNodes()
-	
-	/// how much $ we have
-	static var money = 0
-}
-
+// MARK: - [V] glNodes = GlobalNodes() -
+	/** List of all the lists of nodes: */	var glNodes = GlobalNodes()
 
 
 // MARK: - Tests: -
+	/// Scene 1
+	makeAButton: do {
+		
+	}
 
-/// Scene 1
-makeAButton: do {
-	
-}
+	/// When we change scenes
+	deleteAButton: do {
+		
+	}
 
-// When we change scenes
-deleteAButton: do {
-	
-}
+	/// If score is over 50, change text to "Cash Out"
+	changeAButton: do {
+		
+	}
 
-// If score is over 50, change text to "Cash Out"
-changeAButton: do {
-	
-}
-
-// Maybe update the button every 2 frames?
-update: do {
-	
-	let current_money = 5
-}
+	/// Maybe update the button every 2 frames?
+	update: do {
+		
+		let current_money = 5
+	}
 
 
